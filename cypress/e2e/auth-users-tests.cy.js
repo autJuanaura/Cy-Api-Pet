84033cd59a4deba0d9b9f0f3d48f18a16a77e4c2
juanbig <juannodecoorp@gmail.com>
2024-10-06 13:49:36 -0400

    add update user +++ TEST RESPONSE

--- cypress/e2e/auth-users-tests.cy.js
+++ cypress/e2e/auth-users-tests.cy.js
@@ -32,3 +32,36 @@ it('Login user successfully', () => {
     expect(response.body).to.have.property('message').that.contains('logged in user session:');
   });
 });
+
+
+
+it('Update user successfully', () => {
+  cy.fixture('user.json').then((user) => {
+    // First, create a new user
+    cy.request({
+      method: 'POST',
+      url: '/user',
+      body: user,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      // Then, update the user
+      cy.fixture('update-user.json').then((updatedUser) => {
+        cy.request({
+          method: 'PUT',
+          url: `/user/${response.body.message}`,
+          body: updatedUser,
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then((updateResponse) => {
+          expect(updateResponse.status).to.eq(200);
+          expect(updateResponse.body).to.have.property('code', 200);
+          expect(updateResponse.body).to.have.property('type', 'unknown');
+          expect(updateResponse.body).to.have.property('message').that.eq('9223372036854775807');
+        });
+      });
+    });
+  });
+});
