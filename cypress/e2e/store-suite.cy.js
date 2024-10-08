5982be753270ca96c6f47099f0e37452e4b36d34
juanbig <juannodecoorp@gmail.com>
2024-10-08 05:51:29 -0400

    Store-suite [DONE] :check

--- cypress/e2e/store-suite.cy.js
+++ cypress/e2e/store-suite.cy.js
@@ -1,3 +1,7 @@
+let orderId;
+
+// I've put all possible assertions here, hopefully! - Masha Med
+
 it('Get inventory successfully', () => {
   cy.request({
     method: 'GET',
@@ -20,7 +24,6 @@ it('Get inventory successfully', () => {
   });
 });
 
-
 it('Place order successfully', () => {
   cy.fixture('order.json').then((order) => {
     cy.request({
@@ -41,6 +44,64 @@ it('Place order successfully', () => {
       expect(response.body).to.have.property('shipDate').that.is.a('string');
       expect(response.body).to.have.property('status', 'placed');
       expect(response.body).to.have.property('complete', true);
+      orderId = response.body.id; // store the order ID
+    });
+  });
+});
+
+
+it('Get order successfully by Id', () => {
+  cy.request({
+    method: 'GET',
+    url: `/store/order/${orderId}`,
+  }).then((response) => {
+    expect(response.status).to.eq(200);
+    expect(response.headers).to.have.property('content-type', 'application/json');
+    expect(response.headers).to.have.property('access-control-allow-origin', '*');
+    expect(response.body).to.be.an('object');
+    expect(response.body).to.have.property('id', orderId);
+    expect(response.body).to.have.property('petId', 0);
+    expect(response.body).to.have.property('quantity', 0);
+    expect(response.body).to.have.property('shipDate').that.is.a('string');
+    expect(response.body).to.have.property('status', 'placed');
+    expect(response.body).to.have.property('complete', true);
+  });
+});
+
+it('Delete order successfully', () => {
+  cy.fixture('order.json').then((order) => {
+    cy.request({
+      method: 'POST',
+      url: '/store/order',
+      body: order,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.headers).to.have.property('content-type', 'application/json');
+      expect(response.headers).to.have.property('access-control-allow-origin', '*');
+      expect(response.body).to.be.an('object');
+      expect(response.body).to.have.property('id').that.is.a('number');
+      expect(response.body).to.have.property('petId', 0);
+      expect(response.body).to.have.property('quantity', 0);
+      expect(response.body).to.have.property('shipDate').that.is.a('string');
+      expect(response.body).to.have.property('status', 'placed');
+      expect(response.body).to.have.property('complete', true);
+      const orderId = response.body.id; // store the order ID
+
+      cy.request({
+        method: 'DELETE',
+        url: `/store/order/${orderId}`,
+      }).then((deleteResponse) => {
+        expect(deleteResponse.status).to.eq(200);
+        expect(deleteResponse.headers).to.have.property('content-type', 'application/json');
+        expect(deleteResponse.headers).to.have.property('access-control-allow-origin', '*');
+        expect(deleteResponse.body).to.be.an('object');
+        expect(deleteResponse.body).to.have.property('code', 200);
+        expect(deleteResponse.body).to.have.property('type', 'unknown');
+        expect(deleteResponse.body).to.have.property('message').that.is.a('string');
+      });
     });
   });
 });
