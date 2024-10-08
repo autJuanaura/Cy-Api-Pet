e6643e141cbd60ac84c4759d03e34c1513be320d
juanbig <juannodecoorp@gmail.com>
2024-10-08 05:16:15 -0400

    put order +++

--- cypress/e2e/store-suite.cy.js
+++ cypress/e2e/store-suite.cy.js
@@ -7,7 +7,7 @@ it('Get inventory successfully', () => {
     expect(response.headers).to.have.property('content-type', 'application/json');
     expect(response.headers).to.have.property('access-control-allow-origin', '*');
     expect(response.body).to.be.an('object');
-    expect(response.body).to.have.property('AVAILABLE').that.is.a('number');
+    expect(response.body).to.have.property('available').that.is.a('number');
     expect(response.body).to.have.property('sold').that.is.a('number');
     expect(response.body).to.have.property('string').that.is.a('number');
     expect(response.body).to.have.property('availabe').that.is.a('number');
@@ -17,5 +17,30 @@ it('Get inventory successfully', () => {
     expect(response.body).to.have.property('available').that.is.a('number');
     expect(response.body).to.have.property('Available').that.is.a('number');
     expect(response.body).to.have.property('status').that.is.a('number');
+  });
+});
+
+
+it('Place order successfully', () => {
+  cy.fixture('order.json').then((order) => {
+    cy.request({
+      method: 'POST',
+      url: '/store/order',
+      body: order,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.headers).to.have.property('content-type', 'application/json');
+      expect(response.headers).to.have.property('access-control-allow-origin', '*');
+      expect(response.body).to.be.an('object');
+      expect(response.body).to.have.property('id').that.is.a('number');
+      expect(response.body).to.have.property('petId', 0);
+      expect(response.body).to.have.property('quantity', 0);
+      expect(response.body).to.have.property('shipDate').that.is.a('string');
+      expect(response.body).to.have.property('status', 'placed');
+      expect(response.body).to.have.property('complete', true);
+    });
   });
 });
